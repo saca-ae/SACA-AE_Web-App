9a30411cc339e0fadfe0c2ec3606a62085873054
fgomezvalverde <fafa_gttm@hotmail.com>
2015-04-14 17:52:44 -0600

    WCF Creado y Probado
    para accederlo por browser: http://localhost:50297/ServiceMobile.svc
    
    y contrato : http://localhost:50297/ServiceMobile.svc?wsdl

---
+++ SACAAE/IServiceMobile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SACAAE
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServiceMobile" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServiceMobile
+    {
+        [OperationContract]
+        string DoWork();
+
+        [OperationContract]
+        string HelloWorld();
+
+        [OperationContract]
+        string Login(String pMail);
+        [OperationContract]
+        string GetProyectos(String pMail);
+        [OperationContract]
+        string GetComisiones(String pMail);
+        [OperationContract]
+        string GetCursos(String pMail);
+    }
+}
